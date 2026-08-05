517099ac7486434356ba7cb6cde968ef622a9bee
fryjin <frychengeng@gmail.com>
2026-08-05 22:08:02 +0800

    Delete apps/web/src/phaser/bridges/gameEvents.ts

--- apps/web/src/phaser/bridges/gameEvents.ts
+++
@@ -1,8 +0,0 @@
-import Phaser from 'phaser';
-
-export const gameEvents = new Phaser.Events.EventEmitter();
-
-export const SceneEvents = {
-  pawnMove: 'scene:pawn-move',
-  propertyPurchased: 'scene:property-purchased'
-} as const;
